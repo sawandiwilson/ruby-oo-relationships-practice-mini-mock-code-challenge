9a78cc6b627b7d7306c69e2a3ce5807552f64aca
sawandiwilson <Sawandiwilson@gmail.com>
2020-07-20 14:59:18 -0400

    almost

--- author.rb
+++ author.rb
@@ -1,1 +1,37 @@
+class Author
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def books
+        Book.all.select{|book| book.author == self}  
+    end
+            #should return all of the books an author has written
+
+    def write_book(title, word_count)
+        Book.new(self, title, word_count)
+    end
+            #should take arguments of a title and word count and make a new Book instance associated with this author
+
+    def total_words
+        all_the_words = 0
+        .max {|a, b| a.total_words <=> b.total_words}
+        end  
+        all_the_words
+    end
+
+    def self.most_words
+        word_count_array = self.all.map do |author|
+
+    end
+end
 
--- console.rb
+++ console.rb
@@ -1,6 +1,20 @@
 require 'pry'
 require_relative './book'
 require_relative './author'
+ 
+hansberry = Author.new("Lorraine Hansberry")
+palahniuk = Author.new("Chuck Palahniuk")
+webster = Author.new("Merriam Webster")
+
+raisin = Book.new("Lorraine Hansberry", "A Raisin In The Sun", 400)
+fight = Book.new("Chuck Palhniuk", "Fight Club", 300)
+dictionary = Book.new("The English Dictionary", "Merriam Webster", 1000)
+
+
+
+
+
+
 
 binding.pry
 
